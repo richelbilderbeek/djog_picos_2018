99af3a88ec02d0b8c7ca230dadc0569684ddcebb
Rob Krüger <4958@praedinius.nl>
2019-06-30 14:20:51 +0200

    ben beter dan de rest

--- hoofdscherm.cpp
+++ hoofdscherm.cpp
@@ -18,14 +18,35 @@ void hoofdscherm::keyPressEvent(QKeyEvent * e)
 {
   //Reageer op de toetsen:
   //Omhoog
-
+  if (e->key() == Qt::Key_Up)
+  {
+    ui->speler_1->move(
+      ui->speler_1->x(),
+      ui->speler_1->y() - 10
+    );
+  }
   //Rechts
-
+  if (e->key() == Qt::Key_Right)
+  {
+    ui->speler_1->move(
+      ui->speler_1->x() + 10,
+      ui->speler_1->y()
+      );
+  }
   //Omlaag
-
+  if (e->key() == Qt::Key_Down)
+  {
+    ui->speler_1->move(
+      ui->speler_1->x(),
+      ui->speler_1->y() + 10
+      );
+  }
   //Links
   if (e->key() == Qt::Key_Left)
   {
-    ui->speler_1->setGeometry(ui->speler_1->geometry().adjusted(-64, 0, 0, 0));
+    ui->speler_1->move(
+      ui->speler_1->x() - 10,
+      ui->speler_1->y()
+  );
   }
 }
